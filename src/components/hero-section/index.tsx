b0142dc5297cfd83a440619aebdc7112b55a9df7
jitanshuraut <jayeshastha@gmail.com>
2024-06-27 13:54:01 +0530

    Hero section

---
+++ src/components/hero-section/index.tsx
@@ -0,0 +1,87 @@
+"use client";
+import { AnimatePresence, motion, useInView } from "framer-motion";
+import React from "react";
+import { Button } from "../ui/button";
+import Feeder from "../ui/feeder";
+import Spotlight from "../ui/Spotlight";
+
+export default function HeroSection() {
+  const ref = React.useRef(null);
+  const isInView = useInView(ref);
+
+  const FADE_DOWN_ANIMATION_VARIANTS = {
+    hidden: { opacity: 0, y: -10 },
+    show: { opacity: 1, y: 0, transition: { type: "spring" } },
+  };
+  return (
+    <div className="mx-auto max-w-6xl mt-12 px-6 lg:px-8 bg-transparent relative">
+      <div className="max-w-4xl absolute">
+        <Spotlight fill="#9284D4" />
+      </div>
+      <div className="mx-auto max-w-5xl text-center ">
+        <motion.div
+          initial="hidden"
+          ref={ref}
+          animate={isInView ? "show" : "hidden"}
+          viewport={{ once: true }}
+          variants={{
+            hidden: {},
+            show: {
+              transition: {
+                staggerChildren: 0.15,
+              },
+            },
+          }}
+        >
+          <div className="absolute -top-4 -z-10 flex w-full justify-center">
+            <div className="h-[310px] w-[310px] max-w-full animate-pulse-slow rounded-full bg-[#8678F9] opacity-20 blur-[100px]" />
+          </div>
+          <div className="absolute -top-4 -z-10 flex w-full justify-center">
+            <div className="h-[310px] w-[310px] max-w-full animate-pulse-slow rounded-full bg-[#8678F9] opacity-20 blur-[100px]" />
+          </div>
+
+          <motion.h1
+            variants={FADE_DOWN_ANIMATION_VARIANTS}
+            className="text-4xl font-bold font-headingAlt bg-gradient-to-tr from-purple-300/80 to-white/90 bg-clip-text text-transparent tracking-normal sm:text-7xl  md:text-9xl"
+          >
+            <Feeder feed="Features" />
+            <div className="mb-4"></div>
+            AI-Powered Learning
+          </motion.h1>
+          <motion.p
+            variants={FADE_DOWN_ANIMATION_VARIANTS}
+            className="mt-6 text-lg leading-8"
+          >
+            Transforming prompts into personalized learning experiences with AI
+          </motion.p>
+
+          <motion.div
+            variants={FADE_DOWN_ANIMATION_VARIANTS}
+            className="mt-10 flex items-center justify-center gap-x-6 "
+          >
+            <div className="z-50 w-[60%] mx-auto">
+              <Button className="w-[50%]">Get started</Button>
+            </div>
+          </motion.div>
+        </motion.div>
+      </div>
+      <div className="mt-16 flow-root sm:mt-24">
+        <motion.div
+          className="rounded-md"
+          initial={{ y: 100, opacity: 0 }} // Image starts from 100px below and fully transparent
+          animate={{ y: 0, opacity: 1 }} // Image ends at its original position and fully opaque
+          transition={{ type: "spring", stiffness: 50, damping: 20 }} // transition specifications
+        >
+          <AnimatePresence mode="wait">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.25 }}
+            ></motion.div>
+          </AnimatePresence>
+        </motion.div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/ui/floating-navbar.tsx
@@ -0,0 +1,89 @@
+"use client";
+import React, { useState } from "react";
+import {
+  AnimatePresence,
+  motion,
+  useMotionValueEvent,
+  useScroll,
+} from "framer-motion";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { Icon } from "./card-binary";
+import { ModeToggle } from "../mode-toggle";
+const FloatingNav = ({
+  navItems,
+  className,
+}: {
+  navItems: {
+    name: string;
+    link: string;
+    icon?: JSX.Element;
+  }[];
+  className?: string;
+}) => {
+  const { scrollYProgress } = useScroll();
+
+  const [visible, setVisible] = useState(false);
+
+  useMotionValueEvent(scrollYProgress, "change", (current) => {
+    let direction = current - scrollYProgress.getPrevious();
+
+    if (scrollYProgress.get() < 0.05) {
+      setVisible(true);
+    } else {
+      if (direction < 0) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+    }
+  });
+  return (
+    <AnimatePresence mode="wait">
+      <div className="fixed justify-center z-10 items-center  max-w-fit  top-5 inset-x-0 mx-auto ">
+        <motion.div
+          initial={{
+            opacity: 1,
+            y: -100,
+          }}
+          animate={{
+            y: visible ? 0 : -100,
+            opacity: visible ? 1 : 0,
+          }}
+          transition={{
+            duration: 0.2,
+          }}
+          className="border border-black/[0.2] bg-gradient-to-tr from-purple-400/20 to-transparent dark:border-white/[0.2] flex flex-col items-center justify-center  max-w-sm mx-auto p-4 relative h-[55px] backdrop-blur-2xl  w-full "
+        >
+          <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+          <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+          <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+          <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+
+          <div className="flex pr-2 pl-8 py-2  items-center justify-center space-x-4 border border-transparent">
+            {navItems.map((navItem: any, idx: number) => (
+              <Link
+                key={`link=${idx}`}
+                href={navItem.link}
+                className={cn(
+                  "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+                )}
+              >
+                {/* <span className="block sm:hidden">{navItem.icon}</span> */}
+                <span className="hidden text-sm sm:block mr-[1.20rem]">{navItem.name}</span>  <Icon className=" h-6 w-6 bg-transparent text-gray-300/40 transform rotate-45 mx-20 " />
+              </Link>
+            ))}
+            <ModeToggle/>
+            <button className="relative py-2 px-4 text-sm font-medium text-black rounded-full border dark:text-white border-neutral-200 dark:border-white/[0.2]">
+              <span>Login</span>
+              <span className="absolute inset-x-0 -bottom-px mx-auto w-1/2 h-px bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
+            </button>
+          </div>
+        </motion.div>
+        {/* </motion.div> */}
+      </div>
+    </AnimatePresence>
+  );
+};
+
+export default FloatingNav;
