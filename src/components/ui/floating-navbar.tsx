940342639119108b24f607b1b1fc52f90fe57642
jitanshuraut <jayeshastha@gmail.com>
2024-06-27 17:37:27 +0530

    navbar fixed

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -71,10 +71,13 @@ const FloatingNav = ({
                 )}
               >
                 {/* <span className="block sm:hidden">{navItem.icon}</span> */}
-                <span className="hidden text-sm sm:block mr-[1.20rem]">{navItem.name}</span>  <Icon className=" h-6 w-6 bg-transparent text-gray-300/40 transform rotate-45 mx-20 " />
+                <span className="hidden text-sm sm:block mr-[1.20rem]">
+                  {navItem.name}
+                </span>{" "}
+                <Icon className=" h-6 w-6 bg-transparent text-gray-300/40 transform rotate-45 mx-20 " />
               </Link>
             ))}
-            <ModeToggle/>
+            <ModeToggle />
             <button className="relative py-2 px-4 text-sm font-medium text-black rounded-full border dark:text-white border-neutral-200 dark:border-white/[0.2]">
               <span>Login</span>
               <span className="absolute inset-x-0 -bottom-px mx-auto w-1/2 h-px bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
