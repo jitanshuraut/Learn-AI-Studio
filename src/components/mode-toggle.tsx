95e158813a3c99c51ec68be12c338c00d1d6bb4a
jitanshuraut <jayeshastha@gmail.com>
2024-06-28 17:33:11 +0530

    theme bug solved

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -5,9 +5,10 @@ import { useTheme } from "next-themes"
 export function ModeToggle() {
   const { setTheme, theme } = useTheme()
 
+setTheme("dark")
   return (
     <button
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      
       className="border rounded-md w-6 h-6 flex items-center justify-center">
       <span className="sr-only">Toggle mode</span>
       {theme !== "dark" ? (
--- src/components/ui/feeder.tsx
+++ src/components/ui/feeder.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Feeder = ({feed}: {feed:string} ) => {
   return (
-    <div>
+    <div className="-z-10">
       <button className="bg-purple-700/10 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 tracking-wide  text-white inline-block">
         <span className="absolute inset-0 overflow-hidden rounded-full">
           <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
