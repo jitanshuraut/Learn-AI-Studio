777102e4a423aa1932d8aaf645a29ab187c27f73
jitanshuraut <jayeshastha@gmail.com>
2024-07-19 09:00:17 +0530

    optimize UI and API call

--- src/app/(root)/(routes)/course/[id]/[userId]/page.tsx
+++ src/app/(root)/(routes)/course/[id]/[userId]/page.tsx
@@ -1,53 +1,9 @@
 "use client";
-// import React, { useState, useEffect } from "react";
-// import { usePathname } from "next/navigation";
-
-// type CourseData = {
-//   name: string;
-//   numberofdays: number;
-//   Introduction: string[];
-//   Assessment: string[];
-//   Conclusion: string[];
-//   ReferenceBooks: {
-//     title: string;
-//     version: string;
-//     author: string;
-//     source: string;
-//   }[];
-//   CourseStructureInfluence: string;
-//   [key: string]:
-//     | string
-//     | number
-//     | string[]
-//     | { title: string; version: string; author: string; source: string }[]
-//     | undefined;
-// };
-
-// const extractAndDecodeSegments = (url: string): [string, string] => {
-//   const segments = url.split("/");
-//   const lastSegment = segments[segments.length - 1];
-//   const secondToLastSegment = segments[segments.length - 2];
-//   const decodeBase64 = (str: string) =>
-//     Buffer.from(str, "base64").toString("utf-8");
-
-//   return [decodeBase64(secondToLastSegment), decodeBase64(lastSegment)];
-// };
-
-// const extractDays = (
-//   courseData: CourseData
-// ): { day: string; modules: string[] }[] => {
-//   const days: { day: string; modules: string[] }[] = [];
-
-//   for (const key in courseData) {
-//     if (key.startsWith("Day")) {
-//       const modules = courseData[key] as string[];
-//       days.push({ day: key, modules });
-//     }
-//   }
-
-//   return days;
-// };
-
+import React, { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import { MoveRight } from "lucide-react";
+import { Disc } from "lucide-react";
 const fetchData = async ({
   moduleNumber,
   courseId,
@@ -85,119 +41,6 @@ const fetchData = async ({
     throw error; // Re-throw the error to handle it further up the call stack
   }
 };
-
-// async function Home() {
-//   const pathname = usePathname();
-//   const search = pathname;
-
-//   const [userid, courseid] = extractAndDecodeSegments(search);
-//   const jsonString: string = localStorage.getItem(courseid) || "";
-
-//   const parsedJson = JSON.parse(jsonString);
-//   const { course, coursestructure } = parsedJson;
-
-//   const coursestructure_new = JSON.parse(coursestructure);
-//   const daysWithModules = extractDays(coursestructure_new);
-
-//   const str_mod = daysWithModules[0].modules[0];
-//   const colonIndex = str_mod.indexOf(":");
-//   const content = str_mod.slice(colonIndex + 1).trim();
-//   console.log(content);
-//   const parsedCourseStructure = JSON.parse(coursestructure);
-
-//   Object.keys(parsedCourseStructure).forEach((key) => {
-//     if (key.startsWith("Day")) {
-//       coursestructure_new[key] = parsedCourseStructure[key];
-//     }
-//   });
-
-//   const [selectedDay, setSelectedDay] = useState<number>(1);
-
-//   const nextDay = () => {
-//     const days = Object.keys(daysWithModules);
-//     const currentIndex = selectedDay;
-//     let nextIndex = (currentIndex + 1) % days.length;
-//     if (nextIndex == 0) {
-//       nextIndex = days.length;
-//     }
-//     setSelectedDay(nextIndex);
-//   };
-
-// useEffect(() => {
-//   const fetchCourseData = async () => {
-//     try {
-//       const selectedDayData = daysWithModules.find(
-//         (day) => day.day === `Day ${selectedDay}`
-//       );
-//       if (selectedDayData) {
-//         const module = selectedDayData.modules[0];
-//         const contentIndex = module.indexOf(":");
-//         const content = module.slice(contentIndex + 1).trim();
-
-//         // await fetchData({ module: content, course, userid });
-//         console.log(content);
-//         console.log(course);
-//         console.log(userid);
-//       }
-//     } catch (error) {
-//       console.error("Error fetching course data:", error);
-//     }
-//   };
-
-//   // fetchCourseData();
-//   console.log(selectedDay);
-//   // eslint-disable-next-line react-hooks/exhaustive-deps
-// }, [selectedDay]);
-
-//   return (
-//     <>
-//       <div className="flex">
-//         <div className="w-1/5 z-50 h-screen p-4 bg-primary-foreground">
-//           <h2 className="text-xl font-bold mb-4">Days</h2>
-//           <ul>
-//             {daysWithModules.map((dayData, index) => (
-//               <li
-//                 key={dayData.day}
-//                 className={`p-2 mb-1 rounded-md cursor-pointer ${dayData.day === "Day " + selectedDay ? "bg-[#8678F9] text-white" : "bg-primary-foreground"}`}
-//                 onClick={() => setSelectedDay(index)}
-//               >
-//                 {dayData.day}
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-
-//         <div className="w-3/4 h-screen p-4">
-//           <h2 className="text-2xl font-bold mb-4">Day {selectedDay}</h2>
-//           <ul className="list-disc pl-5">
-//             {daysWithModules
-//               .find((day) => day.day === "Day " + selectedDay)
-//               ?.modules.map((module, index) => (
-//                 <li key={index} className="mb-2">
-//                   {module}
-//                 </li>
-//               ))}
-//           </ul>
-//           <button
-//             className="px-2 py-1 bg-white text-black rounded-md mt-4"
-//             onClick={() => {
-//               nextDay();
-//             }}
-//           >
-//             Next Day
-//           </button>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Home;
-
-import React, { useState, useEffect } from "react";
-import { usePathname } from "next/navigation";
-import Image from "next/image";
-import { MoveRight } from "lucide-react";
 
 type CourseData = {
   name: string;
@@ -265,7 +108,7 @@ const Home: React.FC = () => {
     const { course, coursestructure } = parsedJson;
 
     const coursestructure_new = JSON.parse(coursestructure);
-    console.log(coursestructure_new)
+    console.log(coursestructure_new);
     const daysWithModules = extractDays(coursestructure_new);
 
     const [selectedDay, setSelectedDay] = useState<number>(1);
@@ -346,29 +189,34 @@ const Home: React.FC = () => {
       (data) => data.day === selectedDay
     );
 
-    if (loading) {
+    if (selectedDayModules.length == 0) {
       return (
         <div className="flex h-[100vh] ">
-          <div className="w-1/5 z-50 h-screen p-4 bg-primary-foreground">
+          <div className="w-[10%] z-50 h-screen p-4 bg-primary-foreground">
             <h2 className="text-xl font-bold mb-4">Days</h2>
             <ul>
               {daysWithModules.map((dayData, index) => (
                 <li
                   key={dayData.day}
-                  className={`p-2 mb-1 rounded-md cursor-pointer ${
+                  className={`p-2 flex  mb-1 rounded-md cursor-pointer ${
                     dayData.day === `Day ${selectedDay}`
-                      ? "bg-[#8678F9] text-white"
+                      ? "bg-[#8678F9] text-white justify-start"
                       : "bg-primary-foreground"
                   }`}
                   onClick={() => setSelectedDay(index + 1)}
                 >
-                  {dayData.day}
+                  {dayData.day}{" "}
+                  {dayData.day === `Day ${selectedDay}` ? (
+                    <Disc className="text-sm text-white mx-2" />
+                  ) : (
+                    ""
+                  )}
                 </li>
               ))}
             </ul>
           </div>
 
-          <div className="w-3/4 mx-auto">
+          <div className="w-[85%] mx-auto  p-4">
             <h2 className="text-2xl font-bold mb-4">Day {selectedDay}</h2>
 
             <div className="flex justify-around">
@@ -394,31 +242,41 @@ const Home: React.FC = () => {
       return (
         <>
           <div className="flex h-[100vh]">
-            <div className="w-1/5 z-50 h-screen p-4 bg-primary-foreground">
+            <div className="w-[10%] z-50 h-screen p-4 bg-primary-foreground">
               <h2 className="text-xl font-bold mb-4">Days</h2>
               <ul>
                 {daysWithModules.map((dayData, index) => (
                   <li
                     key={dayData.day}
-                    className={`p-2 mb-1 rounded-md cursor-pointer ${
+                    className={`p-2 mb-1 flex rounded-md cursor-pointer ${
                       dayData.day === `Day ${selectedDay}`
-                        ? "bg-[#8678F9] text-white"
+                        ? "bg-[#8678F9] text-white justify-start "
                         : "bg-primary-foreground"
                     }`}
                     onClick={() => setSelectedDay(index + 1)}
                   >
                     {dayData.day}
+                    {dayData.day === `Day ${selectedDay}` ? (
+                      <Disc className="text-xs  text-white mx-2" />
+                    ) : (
+                      ""
+                    )}
                   </li>
                 ))}
               </ul>
             </div>
 
-            <div className="w-3/4 mx-auto my-2">
-              <h2 className="text-2xl font-bold mb-4">Day {selectedDay} of  <span className="text-[#8678F9]">{coursestructure_new.name}</span> </h2>
+            <div className="w-[85%] mx-auto p-4 ">
+              <h2 className="text-2xl font-bold mb-4">
+                Day {selectedDay} of{" "}
+                <span className="text-[#8678F9]">
+                  {coursestructure_new.name}
+                </span>{" "}
+              </h2>
 
               <div className="flex justify-around">
                 <div className="p-4 h-[85vh] overflow-y-scroll hide-scrollbar w-3/4">
-                  <div className="pl-5">
+                  <div className="">
                     {selectedDayModules.map((moduleData, index) => (
                       <div
                         key={index}
@@ -431,19 +289,23 @@ const Home: React.FC = () => {
                   </div>
                 </div>
 
-                <div className="flex flex-col w-1/5">
+                <div className="flex flex-col items-start w-1/5">
+                  <h1 className="my-2 font-bold">Modules</h1>
                   <div className="bg-[#8678F9] flex flex-col z-50 p-4 rounded-md w-full ">
                     {daysWithModules
                       .find((day) => day.day === `Day ${selectedDay}`)
                       ?.modules.map((module, index) => (
-                        <p key={index} className="mb-2 text-white font-bold">
-                          {module}
+                        <p
+                          key={index}
+                          className="mb-2 text-white font-bold bg-primary-foreground p-2 rounded-md"
+                        >
+                          {module.split(": ")[1]}
                         </p>
                       ))}
                   </div>
 
                   <button
-                    className="px-2 py-1 flex justify-center z-50 bg-white text-black rounded-md mt-4"
+                    className="px-2 w-full py-1 flex justify-center z-50 bg-white text-black rounded-md mt-4"
                     onClick={nextDay}
                   >
                     Next Day <MoveRight className="text-[#8678F9] mx-2" />
